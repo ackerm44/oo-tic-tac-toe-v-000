a70a53eba1b0415bb4b3c9955fae24d1c2566e22
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 19:37:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,4 +39,16 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
 
+  def turn 
+    puts "Please enter 1-9:"
+    input = gets.strip
+    input_to_index
+    if valid_move?(index)
+      move(index, current_player)
+      display_board(board)
+    else
+      turn
+    end
+  end
+
 end
