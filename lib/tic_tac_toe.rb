6835550bec66ce41fe63ec927ffd14446a5e38e7
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 19:27:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,8 +27,8 @@ class TicTacToe
     @index = user_input.to_i - 1
   end
 
-  def move(index, token = "X")
-    @board[@index] = @token
+  def move(board[index], token = "X")
+    @board[index] = @token
   end
 
 end
